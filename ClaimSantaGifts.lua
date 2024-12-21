834836190b816df385ae4a88a6f9a4ea9500b930
Droidlol <dovyllis2@gmail.com>
2024-12-21 12:55:08 -0800

    Update ClaimSantaGifts.lua

--- ClaimSantaGifts.lua
+++ ClaimSantaGifts.lua
@@ -1,9 +1,21 @@
 -- made by discord.gg/virtuehub
 
+-- Locals
+
 local HttpService = game:GetService("HttpService")
 local httpRequest = (syn and syn.request) or (http and http.request) or http_request or (fluxus and fluxus.request)
+local eventdoor = CFrame.new(177.613953, 15.9911404, -143.833618, -0.999994993, 4.56833504e-08, 0.00316533819, 4.53755753e-08, 1, -9.73043797e-08, -0.00316533819, -9.71602674e-08, -0.999994993)
+
+local changeddistance = 5
+local player = game.Players.LocalPlayer
+local character = player.Character or player.CharacterAdded:Wait()
+
+local changedeventdoor = eventdoor + (eventdoor.LookVector * changeddistance)
+
+-- Variables
 
 local claimedParts = {}
+
 
 local function sendInitialWebhook()
     if _G.webhookURL == "" then return end
@@ -38,18 +50,18 @@ local function sendInitialWebhook()
     end
 end
 
-local function sendWebhook(partName)
+local function sendWebhook(partName, status)
     if _G.webhookURL == "" then return end
 
     local payload = {
         username = "Virtue Hub",
         avatar_url = "https://cdn.discordapp.com/attachments/1319992989594030126/1320100746745741403/Virtue_Hub_Logo.png?ex=67685f20&is=67670da0&hm=7a35195b0d68c759320751c5720f09c4a4293fd04ff397b6bcb4f8430a5004c8&",
-        content = "Found A Present",
+        content = status == "claimed" and "Found A Present" or "Present Already Claimed",
         embeds = {
             {
-                title = "Claimed A Present",
+                title = status == "claimed" and "Claimed A Present" or "Skipped Duplicate Present",
                 type = "rich",
-                color = 1127128,
+                color = status == "claimed" and 1127128 or 16711680,
                 fields = {
                     { name = "Present Name: ", value = partName, inline = false },
                     { name = "More Scripts", value = "[Join VirtueHub](https://discord.gg/virtuehub)", inline = false }
@@ -73,6 +85,13 @@ end
 
 sendInitialWebhook()
 
+if character and _G.tptoeventdoor then
+    local humanoidrootpart = character:WaitForChild("HumanoidRootPart")
+    humanoidrootpart.CFrame = changedeventdoor
+end
+
+task.wait(1)
+
 while true do
     for _, part in pairs(game.Workspace:GetDescendants()) do
         if part:FindFirstChild("ClickDetector") and 
@@ -81,16 +100,12 @@ while true do
            part:FindFirstChild("Attachment") and 
            part:FindFirstChild("ParticleEmitter") then
             
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = part.CFrame
             local partName = part.Name
-
-            if not claimedParts[partName] then
-                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = part.CFrame
-                local args = { [1] = partName }
-                game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Christmas Sleigh: Claim"):InvokeServer(unpack(args))
-                claimedParts[partName] = true
-                sendWebhook(partName)
-                wait(_G.invokeDelay)
-            end
+            local args = { [1] = partName }
+            game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Christmas Sleigh: Claim"):InvokeServer(unpack(args))
+            sendWebhook(partName)
+            wait(_G.invokeDelay)
         end
     end
     wait(_G.checkDelay)
